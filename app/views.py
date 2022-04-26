04cecb3b78053ba497b1463c51a00ca505ee112d
sirgama <sirengogamaliel@gmail.com>
2022-04-26 16:31:32 +0300

    add outputmovie title data binding

--- app/views.py
+++ app/views.py
@@ -1,12 +1,16 @@
 from flask import render_template
 from app import app
+from .request import get_movies
 
 #views
 @app.route('/')
 def index():
+    #getting popular movies
+    popular_movies = get_movies('popular')
+    print(popular_movies)
     message = "Hello Flask"
     title = 'Home: Welcome to the bestMovie review website online!'
-    return render_template('index.html',message = message, title = title)
+    return render_template('index.html',message = message, title = title, popular = popular_movies)
 
 @app.route('/movie/<movie_id>')
 def movie(movie_id):

7f7b43a49a864adfd0a3a5a4465bf5583dc7ed34
sirgama <sirengogamaliel@gmail.com>
2022-04-27 15:33:38 +0300

    create a movie unnitest

--- app/request.py
+++ app/request.py
@@ -32,46 +32,6 @@ def get_movies(category):
     return movie_results
 
 
-def get_movie(id):
-    get_movie_details_url = base_url.format(id,api_key)
-
-    with urllib.request.urlopen(get_movie_details_url) as url:
-        movie_details_data = url.read()
-        movie_details_response = json.loads(movie_details_data)
-
-        movie_object = None
-        if movie_details_response:
-            id = movie_details_response.get('id')
-            title = movie_details_response.get('original_title')
-            overview = movie_details_response.get('overview')
-            poster = movie_details_response.get('poster_path')
-            vote_average = movie_details_response.get('vote_average')
-            vote_count = movie_details_response.get('vote_count')
-
-            movie_object = Movie(id,title,overview,poster,vote_average,vote_count)
-
-    return movie_object
-
-
-
-def search_movie(movie_name):
-    search_movie_url = 'https://api.themoviedb.org/3/search/movie?api_key={}&query={}'.format(api_key,movie_name)
-    with urllib.request.urlopen(search_movie_url) as url:
-        search_movie_data = url.read()
-        search_movie_response = json.loads(search_movie_data)
-
-        search_movie_results = None
-
-        if search_movie_response['results']:
-            search_movie_list = search_movie_response['results']
-            search_movie_results = process_results(search_movie_list)
-
-
-    return search_movie_results
-
-
-
-
 def process_results(movie_list):
     '''
     Function  that processes the movie result and transform them to a list of Objects
@@ -89,9 +49,7 @@ def process_results(movie_list):
         vote_average = movie_item.get('vote_average')
         vote_count = movie_item.get('vote_count')
 
-        if poster:
-
-            movie_object = Movie(id,title,overview,poster,vote_average,vote_count)
-            movie_results.append(movie_object)
+        movie_object = Movie(id,title,overview,poster,vote_average,vote_count)
+        movie_results.append(movie_object)
 
     return movie_results

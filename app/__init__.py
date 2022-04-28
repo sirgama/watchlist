c2b377e936c941b9747ebae6c01b08b5d90f6227
sirgama <sirengogamaliel@gmail.com>
2022-04-28 13:42:40 +0300

    Final commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,28 @@
 from flask import Flask
-from .config import DevConfig
+from config import config_options
 from flask_bootstrap import Bootstrap
 
 # Initializing application
-app = Flask(__name__,instance_relative_config = True)
+bootstrap = Bootstrap()
 
-#initializing Flask extensions
-bootstrap = Bootstrap(app)
+def create_app(config_name):
 
-# Setting up configuration
-app.config.from_object(DevConfig)
-app.config.from_pyfile("config.py")
+    app = Flask(__name__)
 
-from app import views
-from app import error
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
+    
+
+    # Initializing flask extensions
+    bootstrap.init_app(app)
+
+    # Will add the views and forms
+      # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    # setting config
+    from .request import configure_request
+    configure_request(app)
+
+    return app
--- app/main/views.py
+++ app/main/views.py
@@ -1,11 +1,15 @@
 from flask import render_template, request, redirect,url_for
-from app import app
-from .request import get_movies, get_movie, search_movie
+
+from . import main
+from ..request import get_movies, get_movie, search_movie
+from ..models import Review
+from .forms import ReviewForm
+
 
 
 
 # Views
-@app.route('/')
+@main.route('/')
 def index():
 
     '''
@@ -34,7 +38,7 @@ def index():
 #     title = f'You are viewing {movie_id}'
 #     return render_template('movie.html',title = title)
 
-@app.route('/movie/<int:id>')
+@main.route('/movie/<int:id>')
 def movie(id):
 
     '''
@@ -42,11 +46,13 @@ def movie(id):
     '''
     movie = get_movie(id)
     title = f'{movie.title}'
+    reviews = Review.get_reviews(movie.id)
 
-    return render_template('movie.html',title = title,movie = movie)
+
+    return render_template('movie.html',title = title,movie = movie,reviews = reviews)
 
 
-@app.route('/search/<movie_name>')
+@main.route('/search/<movie_name>')
 def search(movie_name):
     '''
     View function to display the search results
@@ -56,3 +62,18 @@ def search(movie_name):
     searched_movies = search_movie(movie_name_format)
     title = f'search results for {movie_name}'
     return render_template('search.html',movies = searched_movies)
+
+@main.route('/movie/review/new/<int:id>', methods = ['GET','POST'])
+def new_review(id):
+    form = ReviewForm()
+    movie = get_movie(id)
+
+    if form.validate_on_submit():
+        title = form.title.data
+        review = form.review.data
+        new_review = Review(movie.id,title,movie.poster,review)
+        new_review.save_review()
+        return redirect(url_for('movie',id = movie.id ))
+
+    title = f'{movie.title} review'
+    return render_template('new_review.html',title = title, review_form=form, movie=movie)

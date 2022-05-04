1f6d4738b5598c40a05a3c63c78844e3cec93d90
sirgama <sirengogamaliel@gmail.com>
2022-05-04 22:52:03 +0300

    Add passwrd hashing

--- app/models.py
+++ app/models.py
@@ -1,4 +1,6 @@
 from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
+
 class Movie:
     '''
     Movie class to define Movie Objects
@@ -61,6 +63,25 @@ class Role(db.Model):
     id = db.Column(db.Integer,primary_key = True)
     name = db.Column(db.String(255))
     users = db.relationship('User',backref = 'role',lazy="dynamic")
+       
     
     def __repr__(self):
         return f'User {self.name}'
+    
+    pass_secure  = db.Column(db.String(255))
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure,password)
+    
+
+        
+    

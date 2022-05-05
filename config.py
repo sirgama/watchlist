5a1d538b0af241bbb740c228ac24f871a7489940
sirgama <sirengogamaliel@gmail.com>
2022-05-05 15:42:09 +0300

    Add mail config smtp

--- config.py
+++ config.py
@@ -5,6 +5,16 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI= 'postgresql+psycopg2://Sirgama:Access@localhost/watchlist' 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    SUBJECT_PREFIX = 'Watchlist'
+    SENDER_EMAIL = 'gamaliel.sirengo@student.moringaschool.com'
 
     @staticmethod
     def init_app(app):
@@ -12,15 +22,11 @@ class Config:
 
 
 class ProdConfig(Config):
-    '''
-    '''
-pass
+    pass
 
 
 class DevConfig(Config):
-    '''
-    '''
-DEBUG = True
+    DEBUG = True
 
 config_options = {
 'development':DevConfig,

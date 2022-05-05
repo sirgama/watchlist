f6849a6ccd8c9edb487acf759b0858bc5b54b4ed
sirgama <sirengogamaliel@gmail.com>
2022-05-05 15:28:27 +0300

    Add static methd

--- config.py
+++ config.py
@@ -4,6 +4,11 @@ class Config:
     MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI= 'postgresql+psycopg2://Sirgama:Access@localhost/watchlist' 
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    @staticmethod
+    def init_app(app):
+        pass
 
 
 class ProdConfig(Config):
@@ -11,13 +16,14 @@ class ProdConfig(Config):
     '''
 pass
 
+
 class DevConfig(Config):
     '''
     '''
-    
 DEBUG = True
-    
+
 config_options = {
 'development':DevConfig,
 'production':ProdConfig
+
 }
